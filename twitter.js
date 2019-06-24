a51833e42061565d07721db4deabee7ac2023ae3
Jess Bees <hi@toomanybees.com>
2019-06-23 21:30:11 -0400

    This other function should work on the mobile site

--- twitter.js
+++ twitter.js
@@ -5,6 +5,20 @@ function twitterListener(event) {
     }
 }
 
+// This might work for the new Twitter UI (which is supposedly like the mobile site?)
+function twitterListener2(event) {
+    var parent = event.target.parentElement;
+    if (parent && parent.nodeName === "a") {
+        // If the title is URL-like, then it's the address we want to rewrite.
+        try {
+            var url = new URL(parent.getAttribute("title"));
+            if (url) {
+                parent.setAttribute("href", url.toString());
+            }
+        } catch (_) {}
+    }
+}
+
 var twitterStream = document.getElementById("stream-items-id");
 if (twitterStream && location.hostname.match(/(^|\.)twitter\.com$/)) {
     twitterStream.addEventListener("click", twitterListener);
